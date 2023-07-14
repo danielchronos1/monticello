127f809e02cca701af0e4845cb60654337b79aa4
danielchronos1 <danielciburciu.1@gmail.com>
2023-07-14 16:28:38 +0300

    fixed sections

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-
+//Smmoth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -18,7 +18,7 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: 40.678566188203696, lng: -73.89748768197039 };
+  const position = { lat: 40.677308009365646, lng: -73.8989694329483 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -26,13 +26,14 @@ async function initMap() {
 
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 13,
+    zoom: 14,
     zoomControl: false,
     mapTypeControl: false,
     streetViewControl: false,
     fullscreenControl: false,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: "3869adccf438b2e2",
+    
   });
 
 
@@ -41,13 +42,14 @@ async function initMap() {
 const beachFlagImg = document.createElement("img");
 
 beachFlagImg.src =
-  "../images/footer/marker.png";
+  "/src/images/footer/marker.png";
 
 const beachFlagMarkerView = new google.maps.marker.AdvancedMarkerView({
   map,
   position: { lat: 40.678566188203696, lng: -73.89748768197039 },
   content: beachFlagImg,
   title: "A marker using a custom PNG Image",
+  center: position,
 });
 
   
@@ -55,5 +57,62 @@ const beachFlagMarkerView = new google.maps.marker.AdvancedMarkerView({
 
 initMap();
 
+//
 
+// Get form element and inputs
+const form = document.getElementById('myForm');
+const nameInput = document.getElementById('nameInput');
+const emailInput = document.getElementById('emailInput');
+
+// Add submit event listener to the form
+form.addEventListener('submit', function(event) {
+  // Prevent form submission
+  event.preventDefault();
+
+  // Validate name field
+  if (nameInput.value.trim() === '') {
+    alert('Please enter your name.');
+    nameInput.focus();
+    return;
+  }
+
+  // Validate email field
+  if (emailInput.value.trim() === '') {
+    alert('Please enter your email.');
+    emailInput.focus();
+    return;
+  }
+
+  // Add additional email validation if needed
+  // Example: Regular expression for basic email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailInput.value.trim())) {
+    alert('Please enter a valid email address.');
+    emailInput.focus();
+    return;
+  }
+
+  // If all validations pass, log the form data to the console
+  console.log('Form submitted:');
+  console.log('Name:', nameInput.value.trim());
+  console.log('Email:', emailInput.value.trim());
+
+  // Reset the form after submitting
+  form.reset();
+});
+
+//Change text to icon to btn form
+const button = document.querySelector('.info__btn');
+  const icon = button.querySelector('i');
+  const buttonText = button.querySelector('span');
+
+  button.addEventListener('mouseover', function () {
+    buttonText.style.display = 'none';
+    icon.style.display = 'inline';
+  });
+
+  button.addEventListener('mouseout', function () {
+    buttonText.style.display = 'inline';
+    icon.style.display = 'none';
+  });
 
